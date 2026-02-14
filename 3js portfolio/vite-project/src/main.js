44efcea9ff0bbc85574acee197fe217a7ef5ce3f
Hridayesh Debsarma <hridayeshdebsarma6@gmail.com>
2026-02-14 18:12:50 +0530

    modified the whole project

--- 3js portfolio/vite-project/src/main.js
+++ 3js portfolio/vite-project/src/main.js
@@ -1,15 +1,12 @@
-import './style.css'
+import './style.css';
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+// Setup
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  100
-);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
@@ -17,85 +14,117 @@ const renderer = new THREE.WebGLRenderer({
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+camera.position.setX(-3);
 
-camera.position.z = 20;
+renderer.render(scene, camera);
 
 // Torus
+
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-const material = new THREE.MeshStandardMaterial({
-  color: 0xFF6347,
-});
+const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
 const torus = new THREE.Mesh(geometry, material);
+
 scene.add(torus);
 
 // Lights
-const pointLight = new THREE.PointLight(0xffffff, 1);
+
+const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
-
+const ambientLight = new THREE.AmbientLight(0xffffff);
 scene.add(pointLight, ambientLight);
 
 // Helpers
-const lightHelper = new THREE.PointLightHelper(pointLight);
-const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(lightHelper, gridHelper);
 
-// Orbit Controls
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper)
+
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-function addStar(){
-  const geometry=new THREE.SphereGeometry(0.25,24,24);
-  const material=new THREE.MeshStandardMaterial({color:0xffffff});
+
+function addStar() {
+  const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+  const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
   const star = new THREE.Mesh(geometry, material);
-  const[x,y,z]=Array(3).fill().map(()=>THREE.MathUtils.randFloatSpread(100));
-  star.position.set(x,y,z);
+
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
+
+  star.position.set(x, y, z);
   scene.add(star);
 }
-Array(200).fill().forEach(()=>addStar());
-const spaceTexture=new THREE.TextureLoader().load('space.jpg');
-const hridayeshtexture=new THREE.TextureLoader().load('hridayesh.jpeg');
-const hridayesh=new THREE.Mesh(
-  new THREE.BoxGeometry(3,3,3),
-  new THREE.MeshBasicMaterial({map:hridayeshtexture})
-);
-const moontexture=new THREE.TextureLoader().load('moon.jpg');
-const normalTexture=new THREE.TextureLoader().load('normal.jpg')
-const moon=new THREE.Mesh(
-  new THREE.SphereGeometry(),
+
+Array(200).fill().forEach(addStar);
+
+// Background
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
+scene.background = spaceTexture;
+
+// Avatar
+
+const hridayeshTexture = new THREE.TextureLoader().load('/hero-me.jpg');
+
+const hridayesh = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: hridayeshTexture }));
+
+scene.add(hridayesh);
+
+// Moon
+
+const moonTexture = new THREE.TextureLoader().load('moon.jpg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const moon = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
   new THREE.MeshStandardMaterial({
-     map: moontexture,
-     normalMap:normalTexture
+    map: moonTexture,
+    normalMap: normalTexture,
   })
 );
-moon.position.z=30;
-moon.position.setX(-10);
+
 scene.add(moon);
-scene.add(hridayesh);
-scene.background=spaceTexture;
+
+moon.position.z = 30;
+moon.position.setX(-10);
+
+hridayesh.position.z = -5;
+hridayesh.position.x = 2;
+
+// Scroll Animation
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  moon.rotation.x += 0.05;
+  moon.rotation.y += 0.075;
+  moon.rotation.z += 0.05;
+
+  hridayesh.rotation.y += 0.01;
+  hridayesh.rotation.z += 0.01;
+
+  camera.position.z = t * -0.01;
+  camera.position.x = t * -0.0002;
+  camera.rotation.y = t * -0.0002;
+}
+
+document.body.onscroll = moveCamera;
+moveCamera();
+
+// Animation Loop
 
 function animate() {
   requestAnimationFrame(animate);
 
   torus.rotation.x += 0.01;
   torus.rotation.y += 0.005;
-  torus.rotation.z += 0.001;
+  torus.rotation.z += 0.01;
 
-  controls.update();
+  moon.rotation.x += 0.005;
+
+  // controls.update();
+
   renderer.render(scene, camera);
 }
-function moveCamera(){
-const t=document.body.getBoundingClientRect().top;
-moon.rotation.x+=0.05;
-moon.rotation.x+=0.05;
-moon.rotation.x+=0.05;
-hridayesh.rotation.y+=0.01;
-hridayesh.rotation.y+=0.01;
-hridayesh.rotation.y+=0.01;
-camera.position.z=t* -0.01;
-camera.position.z=t* -0.01;
-camera.position.z=t* -0.01;
-}
-document.body.onscroll=moveCamera
 
 animate();
